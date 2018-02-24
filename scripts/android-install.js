ab7ba73c03d5ddd487cf23c222c3f7332664bc7f
zhengxiangyang <zxy@xinshi.net>
2018-02-24 11:29:01 +0800

    图片做了修改
    做了一些修改

--- scripts/android-install.js
+++ scripts/android-install.js
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-    var path         = context.requireCordovaModule('path'),
-        fs           = context.requireCordovaModule('fs'),
-        shell        = context.requireCordovaModule('shelljs'),
-        projectRoot  = context.opts.projectRoot,
-        ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser'),
-        config       = new ConfigParser(path.join(context.opts.projectRoot, "config.xml")),
+    var path        = context.requireCordovaModule('path'),
+        fs          = context.requireCordovaModule('fs'),
+        shell       = context.requireCordovaModule('shelljs'),
+        projectRoot = context.opts.projectRoot;
+
+    var ConfigParser = null;
+    try {
+        ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser;
+    } catch(e) {
+        // fallback
+        ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser');
+    }
+
+    var config      = new ConfigParser(path.join(context.opts.projectRoot, "config.xml")),
         packageName = config.android_packageName() || config.packageName();
 
+    // replace dash (-) with underscore (_)
+    packageName = packageName.replace(/-/g , "_");
+    
     console.info("Running android-install.Hook: " + context.hook + ", Package: " + packageName + ", Path: " + projectRoot + ".");
 
     if (!packageName) {
@@ -23,25 +34,29 @@ module.exports = function (context) {
     }
 
     var targetDir  = path.join(projectRoot, "platforms", "android", "src", packageName.replace(/\./g, path.sep), "wxapi");
-        targetFile = path.join(targetDir, "WXEntryActivity.java");
+        targetFiles = ["EntryActivity.java", "WXEntryActivity.java", "WXPayEntryActivity.java"];
 
     if (['after_plugin_add', 'after_plugin_install', 'after_platform_add'].indexOf(context.hook) === -1) {
         // remove it?
-        try {
-            fs.unlinkSync(targetFile);
-        } catch (err) {}
+        targetFiles.forEach(function (f) {
+            try {
+                fs.unlinkSync(path.join(targetDir, f));
+            } catch (err) {}
+        });
     } else {
         // create directory
         shell.mkdir('-p', targetDir);
 
         // sync the content
-        fs.readFile(path.join(context.opts.plugin.dir, 'src', 'android', 'WXEntryActivity.java'), {encoding: 'utf-8'}, function (err, data) {
-            if (err) {
-                throw err;
-            }
+        targetFiles.forEach(function (f) {
+            fs.readFile(path.join(context.opts.plugin.dir, 'src', 'android', f), {encoding: 'utf-8'}, function (err, data) {
+                if (err) {
+                    throw err;
+                }
 
-            data = data.replace(/^package __PACKAGE_NAME__;/m, 'package ' + packageName + '.wxapi;');
-            fs.writeFileSync(targetFile, data);
+                data = data.replace(/^package __PACKAGE_NAME__;/m, 'package ' + packageName + '.wxapi;');
+                fs.writeFileSync(path.join(targetDir, f), data);
+            });
         });
     }
 };
